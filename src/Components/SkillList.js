c99d98b42e40f54e49b62ddeba0bca03e4c7de26
Nelson Diaz <abnerdc86@gmail.com>
2019-07-15 18:25:05 -0400

    animating tiles #1

--- src/Components/SkillList.js
+++ src/Components/SkillList.js
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const renderSkill = (image, index) => {
+const Skill = ({ image, skillId }) => {
   return (
-    // <div key={index} className="col-3-md col-6-sm col-12-xs ">
-    <div className="square-medium img-fluid">
+    <div id={skillId} className="square-medium img-fluid">
       <img src={image.icon} className="vector p-4" />
     </div>
-
-    // </div>
   );
 };
 
 const SkillList = ({ skills }) => {
+  const [active, setActive] = useState(null);
+  const onClickSkill = id => setActive(id);
+  const onBlurSkill = id => setActive(null);
   return (
     <div>
       <h2>TECHNICAL SKILLS</h2>
       <div>
         <div className="thumbnail-list">
-          {skills.map((skill, index) => {
-            return renderSkill(skill, index);
-          })}
+          {skills.map((skill, index) => (
+            <Skill
+              image={skill}
+              key={index}
+              skillId={index}
+              onClickSkill={() => onClickSkill(index)}
+            />
+          ))}
         </div>
       </div>
     </div>
