7c8a9ed99af476e6479acd552feb1e1b59f33e99
Nelson Diaz <abnerdc86@gmail.com>
2019-07-16 08:52:19 -0400

    working

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,19 +1,21 @@
-import React, { Component } from "react";
-import Home from "./Pages/Home";
+import React, { Component } from 'react';
+import Home from './Pages/Home';
 // import Portfolio from './Pages/Portfolio';
-import SkillList from "../Components/SkillList";
-import data from "../bio-data.json";
+import SkillList from '../Components/SkillList';
+import data from '../bio-data.json';
 
-import Resume from "./Pages/Resume";
+import Resume from './Pages/Resume';
 
 class Content extends Component {
+  state = { skills: data.skills.filter(skill => skill.show) };
+
   render() {
     return (
       <div className="content">
         <Home />
         {/* <Portfolio /> */}
         <Resume />
-        <SkillList skills={data.skills} />
+        <SkillList skills={this.state.skills} />
         <Home />
       </div>
     );
--- src/Components/SkillList.js
+++ src/Components/SkillList.js
@@ -1,18 +1,48 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-const Skill = ({ image, skillId }) => {
+const Skill = ({ image, skillId, onClickSkill, active }) => {
   return (
-    <div id={skillId} className="square-medium img-fluid">
-      <img src={image.icon} className="vector p-4" />
+    <div id={skillId} onClick={onClickSkill} className="square-medium img-fluid">
+      {!active ? (
+        <img src={image.icon} className="vector p-4" />
+      ) : (
+        <div className="skill-label-card p-4">
+          <div className="skill-label">
+            <div className="skill-title">
+              <h5>{image.name}</h5>
+              <h6>{image.type}</h6>
+              {/* <h6>{image.score}%</h6> */}
+              <div class="progress" style={{ height: '1rem' }}>
+                <div
+                  class="progress-bar"
+                  style={{ width: `${image.score}%` }}
+                  aria-valuemin={0}
+                  aria-valuemax={100}
+                >
+                  {image.score}%
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-    // </div>
   );
 };
 
 const SkillList = ({ skills }) => {
   const [active, setActive] = useState(null);
-  const onClickSkill = id => setActive(id);
-  const onBlurSkill = id => setActive(null);
+
+  const onClickSkill = id => {
+    if (!active || active !== id) {
+      return setActive(id);
+    }
+    if (active === id) {
+      return setActive(null);
+    }
+  };
+  // const onBlurSkill = id => setActive(null);
+
   return (
     <div>
       <h2>TECHNICAL SKILLS</h2>
@@ -24,6 +54,7 @@ const SkillList = ({ skills }) => {
               key={index}
               skillId={index}
               onClickSkill={() => onClickSkill(index)}
+              active={active === index ? true : false}
             />
           ))}
         </div>
