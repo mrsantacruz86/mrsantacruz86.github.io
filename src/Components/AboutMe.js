08f04458d946f968428f04bc729a69856900c40f
Nelson Diaz <abnerdc86@gmail.com>
2019-07-24 18:25:10 -0400

    working on the about me section #1

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,29 +1,29 @@
-import React from "react";
-import SocialIcons from "./SocialIcons";
+import React from 'react';
+import SocialIcons from './SocialIcons';
+
+const profile = './assets/images/profile-picture-480x480-web.png';
 
 const AboutMe = ({ bio, networks }) => {
   return (
-    <div className="personal-info">
-      <img
+    <div
+      className="about-me my-5"
+      style={{
+        backgroundImage: `url(${profile}) linear-gradient(to left, black, white )`
+      }}
+    >
+      {/* <img
         src="assets/images/profile-picture-480x480-web.png"
         alt="Profile"
-        className="rounded-circle img-responsive profile-picture"
-      />
-      <p className="font-italic">
-        "Web Engineer with versatile experience in Front and Back-end
-        Technologies, committed to integrate functionality and beauty as
-        essential parts of my work."
-      </p>
+        className="img-responsive profile-picture"
+      /> */}
+      <div className="block-content">
+        <h2 id="about">About Me</h2>
 
-      <hr />
-
-      <div className="justify-content-md-center">
-        <SocialIcons networks={networks} />
+        <p>{bio}</p>
+        <div className="justify-content-md-center">
+          <SocialIcons networks={networks} />
+        </div>
       </div>
-      <hr />
-      <h2 id="about">About Me</h2>
-      <hr />
-      <p>{bio}</p>
     </div>
   );
 };
