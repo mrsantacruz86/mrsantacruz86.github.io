1b28d81d159490020252bb21f48353ddc6b7fe30
Nelson Diaz <abnerdc86@gmail.com>
2019-07-26 23:11:22 -0400

    dynamically load experience.

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -1,28 +1,22 @@
 import React from 'react';
 
-const Experience = props => {
-  const { experience } = props;
+const Experience = ({ experience }) => {
   return (
     <div className="container">
-      <h1 className="experience display-4">Experience</h1>
+      <h1 className="experience display-4 mt-5">Experience</h1>
       <div>
         {experience.map((job, index) => (
           <div className="job-item" key={index}>
             <h4 className="title">
               <div className="checkpoint colored" />
-              Software Developer
+              {job.title}
             </h4>
             <div className="details">
               <div className="d-flex justify-content-between">
-                <h6 className="company">HIS HOUSE CHILDREN'S HOME</h6>
-                <div className="dates">May 2017 - Present</div>
+                <h6 className="company">{job.company.toUpperCase()}</h6>
+                <div className="dates">{`${job.start} - ${job.end}`}</div>
               </div>
-              <div className="description">
-                Created web app that keeps audits records and generates reports.
-                The app was a secured API using JWT and the UI is built with
-                ReactJS. The app has helped the company to reduce paperwork and
-                boost efficiency.
-              </div>
+              <div className="description">{job.description}</div>
             </div>
           </div>
         ))}
